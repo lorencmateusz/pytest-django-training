89effceec33f9f5357a40b67f785962777a9370a
Mateusz Lorenc <MALOR@orsted.com>
2022-09-08 13:17:36 +0200

    added urls and api

--- pytestCourse/companies/views.py
+++ pytestCourse/companies/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import CompanySerializer
+from .models import Company
 
-# Create your views here.
+
+class CompanyViewSet(ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all().order_by("-last_update")
