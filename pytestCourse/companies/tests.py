b3ab74db1193e3428627d00f0b95f31bd9fc2777
Mateusz Lorenc <MALOR@orsted.com>
2022-09-12 13:40:21 +0200

    added api testing

--- pytestCourse/companies/tests.py
+++ pytestCourse/companies/tests.py
@@ -16,16 +16,54 @@ django.setup()
 
 
 @pytest.mark.django_db
-class TestGetCompanies(TestCase):
+class SetupTestCompanies(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.companies_url = reverse("companies-list")
+
+    def tearDown(self) -> None:
+        pass
+
+
+@pytest.mark.django_db
+class TestGetCompanies(SetupTestCompanies):
+
     def test_zero_companies_return_empty_list(self) -> None:
-        client = Client()
-        companies_url = reverse("companies-list")
-        response = client.get(companies_url)
+        response = self.client.get(self.companies_url)
         self.assertEqual(response.status_code, 200)
+        print(response.status_code)
 
     def test_one_company_return_should_succeed(self) -> None:
-        client = Client()
         amazon = Company.objects.create(name='Amazon')
-        companies_url = reverse('companies-list')
-        response = client.get(companies_url)
-        response_content = json.loads(response.content)[0]
+        response = self.client.get(self.companies_url)
+        response_content = json.loads(response.content)[0]['name']
+        assert response_content == 'Amazon'
+
+
+@pytest.mark.django_db
+class TestPostCompanies(SetupTestCompanies):
+    def test_create_company_without_arguments_should_fail(self):
+        response = self.client.post(path=self.companies_url)
+        response_status = response.status_code
+        response_content = json.loads(response.content)['name']
+        assert response_content == ['This field is required.']
+        assert response_status == 400
+
+    def test_add_existing_company_should_fail(self) -> None:
+        Company.objects.create(name='Apple')
+        response = self.client.post(path=self.companies_url, data={'name' : 'Apple'})
+        response_status = response.status_code
+        response_content = json.loads(response.content)['name']
+        assert response_content == ['company with this name already exists.']
+        assert response_status == 400
+
+    def test_add_company_with_only_name_should_return_defaults(self) -> None:
+        response = self.client.post(path=self.companies_url, data={'name': 'test company'})
+        response_status = response.status_code
+        response_content = json.loads(response.content)
+        assert response_content['name'] == 'test company'
+        assert response_content['status'] == 'Hiring'
+        assert response_content['notes'] == ''
+        assert response_status == 201
+
+
